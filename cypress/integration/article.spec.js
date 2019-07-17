fff999041354f464f3c1cff90b61b8df0378cde8
Simon Davey <simonr.davey@gmail.com>
2019-07-17 11:57:51 +0100

    Adds Cypress tests for viewing a single article.

---
+++ cypress/integration/article.spec.js
@@ -0,0 +1,31 @@
+const BASE_URL = 'http://localhost:3000/';
+
+describe('/', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route('GET', '/api/articles/*', 'fx:article.json').as('getArticle');
+    cy.route('GET', '/api/articles/*/comments', 'fx:comments.json').as('getComments');
+    cy.visit(BASE_URL + 'mitch/1');
+  });
+  it('should display a loading message until the article has been retrieved', () => {
+    cy.get('[data-cy=loading]');
+  });
+  it('should display an error message on a bad response', () => {
+    cy.get('[data-cy=loading]');
+    cy.route('GET', '/api/articles/101', {}).as('getBadArticle');
+    cy.visit(BASE_URL + 'mitch/101');
+    cy.get('[data-cy=error]');
+  });
+  it('should display a title, author and body when the article has been retrieved', () => {
+    cy.get('[data-cy=title]');
+    cy.get('[data-cy=author]');
+    cy.get('[data-cy=body]');
+  });
+  it('should display a loading message until the comments have been retrieved', () => {
+    cy.get('[data-cy=loading-comments]');
+  });
+  it('should display the comments for the article when they have been retrieved', () => {
+    cy.get('[data-cy=comment-body]').should('have.length', 3);
+    cy.get('[data-cy=comment-author]').should('have.length', 3);
+  });
+});
