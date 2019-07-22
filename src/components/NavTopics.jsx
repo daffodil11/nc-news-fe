11d598f745443771c9caf5431f0159ead98314e3
Simon Davey <simonr.davey@gmail.com>
2019-07-22 10:48:05 +0100

    Extracts the icon on the nav toggle button into a component and adds a welcome message in the nav bar.

--- src/components/NavTopics.jsx
+++ src/components/NavTopics.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './NavTopics.css';
 import * as api from '../utils/api.js';
+import NavToggleButton from './NavToggleIcon';
 import { Link } from '@reach/router';
 
 class NavTopics extends Component {
@@ -12,13 +13,20 @@ class NavTopics extends Component {
   render() {
     const { topics, open } = this.state;
     const { URL } = document;
+    const username = "daffodil11";
     return (
-      <nav className={open ? "nav-open" : "nav-closed"}>
-          {(document.body.clientWidth < 600) && <a className="nav-button" onClick={this.toggleMenu} ><svg xmlns='http://www.w3.org/2000/svg' version="1.1" width='20px' height='20px' >{ open ? <><line x1="3" y1="17" x2="17" y2="3" /> <line x1="3" y1="3" x2="17" y2="17" /></> : <><line x1="2" y1="2" x2="18" y2="2" /> <line x1="2" y1="10" x2="18" y2="10" /> <line x1="2" y1="18" x2="18" y2="18" /></> }</svg></a>}
-        <Link className="nav-button" to="/" onClick={this.toggleMenu} >all</Link>
-        {topics.map(({ slug, description }) => (
-          <Link to={'/' + slug} title={description} className={`nav-button${URL.includes(slug) ? " current" : ""}`} data-cy="topic-button" key={slug} onClick={this.toggleMenu} >{slug}</Link>))}
-      </nav>
+      <div className="nav-container">
+        <nav className={open ? "nav-open" : "nav-closed"}>
+            {(document.body.clientWidth < 600) && <a id="nav-toggle" className="nav-button" onClick={this.toggleMenu} ><NavToggleButton open={open} /></a>}
+          <Link className="nav-button" to="/" onClick={this.toggleMenu} >all</Link>
+          {topics.map(({ slug, description }) => (
+            <Link to={'/' + slug} title={description} className={`nav-button${URL.includes(slug) ? " current" : ""}`} data-cy="topic-button" key={slug} onClick={this.toggleMenu} >{slug}</Link>))}
+        </nav>
+        <div className="user-welcome">
+          <span>Welcome,</span>
+          <span className="username">{username}!</span>
+        </div>
+      </div>
     );
   }
 
