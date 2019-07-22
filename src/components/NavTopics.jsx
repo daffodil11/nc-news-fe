bbee95dcf9284eb1e291a72fa841549761209534
Simon Davey <simonr.davey@gmail.com>
2019-07-22 09:02:34 +0100

    Changes navbar to have a dedicated open/close button with conditionally rendered SVG.

--- src/components/NavTopics.jsx
+++ src/components/NavTopics.jsx
@@ -14,10 +14,10 @@ class NavTopics extends Component {
     const { URL } = document;
     return (
       <nav className={open ? "nav-open" : "nav-closed"}>
-          {open ? <Link className="nav-button" to="/" onClick={this.toggleMenu} >all</Link> : <a className="nav-button" onClick={this.toggleMenu} >Topics</a>}
-            {topics.map(({ slug, description }) => (
-              <Link to={'/' + slug} title={description} className={`nav-button${URL.includes(slug) ? " current" : ""}`} data-cy="topic-button" key={slug} onClick={this.toggleMenu} >{slug}</Link>
-                ))}
+          {(document.body.clientWidth < 600) && <a className="nav-button" onClick={this.toggleMenu} ><svg xmlns='http://www.w3.org/2000/svg' version="1.1" width='20px' height='20px' >{ open ? <><line x1="3" y1="17" x2="17" y2="3" /> <line x1="3" y1="3" x2="17" y2="17" /></> : <><line x1="2" y1="2" x2="18" y2="2" /> <line x1="2" y1="10" x2="18" y2="10" /> <line x1="2" y1="18" x2="18" y2="18" /></> }</svg></a>}
+        <Link className="nav-button" to="/" onClick={this.toggleMenu} >all</Link>
+        {topics.map(({ slug, description }) => (
+          <Link to={'/' + slug} title={description} className={`nav-button${URL.includes(slug) ? " current" : ""}`} data-cy="topic-button" key={slug} onClick={this.toggleMenu} >{slug}</Link>))}
       </nav>
     );
   }
